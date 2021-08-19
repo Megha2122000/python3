f1292b4d2448e0d9ff787a9619859e2e52668624
Megha2122000 <84174200+Megha2122000@users.noreply.github.com>
2021-08-19 16:01:04 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 19 15:30:29 2021
+
+@author: Comp
+"""
+
+# Python program to print positive Numbers in a List 
+
+  
+# list of numbers 
+
+list1 = [-12 , -7 , 5 ,64 , -14] 
+
+  
+# iterating each number in list 
+
+for num in list1: 
+
+      
+
+    # checking condition 
+
+    if num >= 0: 
+
+       print(num, end = " ")
